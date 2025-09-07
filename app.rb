b657f284a9b95ff0e9dddffc05658a52ae9fff99
Arsalan Bardsiri <35473718+arsalanbardsiri@users.noreply.github.com>
2025-09-07 20:33:43 +0000

    final

--- app.rb
+++ app.rb
@@ -1,20 +1,47 @@
 require "sinatra"
 require "sinatra/reloader"
 
+class Game
+  POSSIBILITIES = { "rock" => "scissors", "scissors" => "paper", "paper" => "rock" }
+  OPTIONS = POSSIBILITIES.keys
+
+  def our_choice(choice)
+    OPTIONS.include?(choice) ? choice : raise(ArgumentError, "Invalid choice")
+  end
+
+  def their_choice
+    OPTIONS.sample
+  end
+
+  def evaluate(choice_one, choice_two)
+    return "We tied!" if choice_one == choice_two
+    POSSIBILITIES[choice_one] == choice_two ? "We won!" : "We lost!"
+  end
+end
+
+the_game = Game.new
+
 get("/") do
-  # "
-  # <h1>Welcome to your Sinatra App!</h1>
-  # <p>Define some routes in app.rb</p>
-  # "
-  erb(:home)
+  erb :home
 end
 
 get("/rock") do
-  "<p>Rock</p>"
+  @us     = the_game.our_choice("rock")
+  @them   = the_game.their_choice
+  @result = the_game.evaluate(@us, @them)
+  erb :rock
 end
+
 get("/paper") do
-  "<p>Paper</p>"
+  @us     = the_game.our_choice("paper")
+  @them   = the_game.their_choice
+  @result = the_game.evaluate(@us, @them)
+  erb :paper
 end
+
 get("/scissors") do
-  "<p>scissors</p>"
+  @us     = the_game.our_choice("scissors")
+  @them   = the_game.their_choice
+  @result = the_game.evaluate(@us, @them)
+  erb :scissors
 end
