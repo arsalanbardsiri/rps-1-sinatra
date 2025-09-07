6f67ee2318c8379353133ddca66245962af73475
Arsalan Bardsiri <35473718+arsalanbardsiri@users.noreply.github.com>
2025-09-07 19:38:14 +0000

    R,P,S views - Home page content - R,P,S routes

--- app.rb
+++ app.rb
@@ -2,8 +2,19 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  # "
+  # <h1>Welcome to your Sinatra App!</h1>
+  # <p>Define some routes in app.rb</p>
+  # "
+  erb(:home)
+end
+
+get("/rock") do
+  "<p>Rock</p>"
+end
+get("/paper") do
+  "<p>Paper</p>"
+end
+get("/scissors") do
+  "<p>scissors</p>"
 end
